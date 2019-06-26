539fb0dd4b7aa0a35b6958787db804d3db9f3c91
Laisky <ppcelery@gmail.com>
2019-06-26 14:56:09 +0800

    feat: add `DivideJWT`

--- encrypt.go
+++ encrypt.go
@@ -38,29 +38,58 @@ import (
 var (
 	// JWTSigningMethod default method to signing
 	JWTSigningMethod = jwt.SigningMethodHS512
-	// JWTExpiresLayout default expires date stores in payload
-	JWTExpiresLayout = time.RFC3339
 )
 
+const (
+	// JWTExpiresLayout default expires date stores in payload
+	JWTExpiresLayout = time.RFC3339
+	// JWTUserIDKey default key of user_id stores in token payload
+	JWTUserIDKey = "uid"
+	// JWTExpiresAtKey default key of expires_at stores in token payload
+	JWTExpiresAtKey = "exp"
+)
+
+type baseJWT struct {
+	JWTSigningMethod              *jwt.SigningMethodHMAC
+	JWTUserIDKey, JWTExpiresAtKey string
+}
+
+// checkExpiresValid return the bool whether the `expires_at` is not expired
+func (j *baseJWT) checkExpiresValid(now time.Time, expiresAtI interface{}) (ok bool, err error) {
+	expiresAt, ok := expiresAtI.(string)
+	if !ok {
+		return false, fmt.Errorf("`%v` is not string", j.JWTExpiresAtKey)
+	}
+	tokenT, err := time.Parse(JWTExpiresLayout, expiresAt)
+	if err != nil {
+		return false, errors.Wrap(err, "try to parse token expires_at error")
+	}
+
+	return now.Before(tokenT), nil
+}
+
 // JWT struct to generate and validate jwt tokens
+//
+// use a global uniform secret to signing all token.
 type JWT struct {
 	*JwtCfg
 }
 
 // JwtCfg configuration of JWT
 type JwtCfg struct {
-	Secret                  []byte
-	JWTSigningMethod        *jwt.SigningMethodHMAC
-	UserIDKey, ExpiresAtKey string
+	baseJWT
+	Secret []byte
 }
 
 // NewJWTCfg create new JwtCfg  with secret
 func NewJWTCfg(secret []byte) *JwtCfg {
 	return &JwtCfg{
-		Secret:           secret,
-		JWTSigningMethod: jwt.SigningMethodHS512,
-		UserIDKey:        "uid",
-		ExpiresAtKey:     "expires_at",
+		Secret: secret,
+		baseJWT: baseJWT{
+			JWTSigningMethod: JWTSigningMethod,
+			JWTUserIDKey:     JWTUserIDKey,
+			JWTExpiresAtKey:  JWTExpiresAtKey,
+		},
 	}
 }
 
@@ -70,19 +99,21 @@ func NewJWT(cfg *JwtCfg) (*JWT, error) {
 		return nil, errors.New("jwtCfg.Secret should not be empty")
 	}
 
+	jwt.TimeFunc = Clock.GetUTCNow
+
 	return &JWT{
 		JwtCfg: cfg,
 	}, nil
 }
 
-// Setup (deprecated) initialize JWT
-func (j *JWT) Setup(secret string) {
-	// const key names
-	j.ExpiresAtKey = "expires_at"
-	j.UserIDKey = "uid"
+// // Setup (deprecated) initialize JWT
+// func (j *JWT) Setup(secret string) {
+// 	// const key names
+// 	j.JWTExpiresAtKey = "expires_at"
+// 	j.JWTUserIDKey = "uid"
 
-	j.Secret = []byte(secret)
-}
+// 	j.Secret = []byte(secret)
+// }
 
 // Generate (Deprecated) generate JWT token.
 // old interface
@@ -101,15 +132,31 @@ func (j *JWT) Generate(expiresAt int64, payload map[string]interface{}) (string,
 	return tokenStr, nil
 }
 
-// GenerateToken generate JWT token.
-// do not use `expires_at` & `uid` as keys.
-func (j *JWT) GenerateToken(userID string, expiresAt time.Time, payload map[string]interface{}) (tokenStr string, err error) {
+// // GenerateToken generate JWT token.
+// // do not use `expires_at` & `uid` as keys.
+// func (j *JWT) GenerateToken(userID string, expiresAt time.Time, payload map[string]interface{}) (tokenStr string, err error) {
+// 	jwtPayload := jwt.MapClaims{}
+// 	for k, v := range payload {
+// 		jwtPayload[k] = v
+// 	}
+// 	jwtPayload[j.JWTExpiresAtKey] = expiresAt.Unix()
+// 	jwtPayload[j.JWTUserIDKey] = userID
+
+// 	token := jwt.NewWithClaims(JWTSigningMethod, jwtPayload)
+// 	if tokenStr, err = token.SignedString(j.Secret); err != nil {
+// 		return "", errors.Wrap(err, "try to signed token got error")
+// 	}
+// 	return tokenStr, nil
+// }
+
+// GenerateToken generate JWT token with userID(interface{})
+func (j *JWT) GenerateToken(userID interface{}, expiresAt time.Time, payload map[string]interface{}) (tokenStr string, err error) {
 	jwtPayload := jwt.MapClaims{}
 	for k, v := range payload {
 		jwtPayload[k] = v
 	}
-	jwtPayload[j.ExpiresAtKey] = expiresAt.Format(JWTExpiresLayout)
-	jwtPayload[j.UserIDKey] = userID
+	jwtPayload[j.JWTExpiresAtKey] = expiresAt.Unix()
+	jwtPayload[j.JWTUserIDKey] = userID
 
 	token := jwt.NewWithClaims(JWTSigningMethod, jwtPayload)
 	if tokenStr, err = token.SignedString(j.Secret); err != nil {
@@ -118,56 +165,80 @@ func (j *JWT) GenerateToken(userID string, expiresAt time.Time, payload map[stri
 	return tokenStr, nil
 }
 
-// checkExpiresValid return the bool whether the `expires_at` is not expired
-func (j *JWT) checkExpiresValid(now time.Time, expiresAtI interface{}) (ok bool, err error) {
-	expiresAt, ok := expiresAtI.(string)
-	if !ok {
-		return false, fmt.Errorf("`%v` is not string", j.ExpiresAtKey)
-	}
-	tokenT, err := time.Parse(JWTExpiresLayout, expiresAt)
-	if err != nil {
-		return false, errors.Wrap(err, "try to parse token expires_at error")
-	}
+// // Validate (deprecated) validate the token and return the payload
+// //
+// // if token is invalidate, err will not be nil.
+// func (j *JWT) Validate(tokenStr string) (payload map[string]interface{}, err error) {
+// 	Logger.Debug("Validate for token", zap.String("tokenStr", tokenStr))
+// 	var (
+// 		claims jwt.MapClaims
+// 		ok     bool
+// 	)
+// 	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+// 		// Don't forget to validate the alg is what you expect:
+// 		if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || method != j.JWTSigningMethod {
+// 			return nil, errors.New("JWT method not allowd")
+// 		}
+// 		return j.Secret, nil
+// 	})
+// 	if err != nil || !token.Valid {
+// 		// delay return after got payload
+// 		err = errors.Wrap(err, "token invalidate")
+// 	}
 
-	return now.Before(tokenT), nil
-}
+// 	payload = map[string]interface{}{}
+// 	if claims, ok = token.Claims.(jwt.MapClaims); !ok {
+// 		return nil, errors.New("payload type not match `map[string]interface{}`")
+// 	}
+// 	for k, v := range claims {
+// 		payload[k] = v
+// 	}
+// 	if err != nil { // no need for furthur validate
+// 		return payload, err
+// 	}
+
+// 	if !claims.VerifyExpiresAt(Clock.GetUTCNow().Unix(), true) {
+// 		return payload, fmt.Errorf("token expired")
+// 	}
+// 	if _, ok = payload[j.JWTUserIDKey]; !ok {
+// 		return payload, fmt.Errorf("token must contains `%v`", j.JWTUserIDKey)
+// 	}
+
+// 	return payload, err
+// }
 
 // Validate validate the token and return the payload
-func (j *JWT) Validate(tokenStr string) (payload map[string]interface{}, err error) {
+//
+// if token is invalidate, err will not be nil.
+func (j *JWT) Validate(tokenStr string) (payload jwt.MapClaims, err error) {
 	Logger.Debug("Validate for token", zap.String("tokenStr", tokenStr))
-	payload = map[string]interface{}{}
 	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 		// Don't forget to validate the alg is what you expect:
-		if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || method != JWTSigningMethod {
+		if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || method != j.JWTSigningMethod {
 			return nil, errors.New("JWT method not allowd")
 		}
 		return j.Secret, nil
 	})
+	if err != nil || !token.Valid {
+		// return after got payload
+		err = errors.Wrap(err, "token invalidate")
+	}
+
+	var ok bool
+	if payload, ok = token.Claims.(jwt.MapClaims); !ok {
+		return nil, errors.New("payload type not match `map[string]interface{}`")
+	}
 	if err != nil {
-		return nil, errors.Wrap(err, "token validate error")
+		return payload, err
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		for k, v := range claims {
-			payload[k] = v
-		}
-		if _, ok := payload[j.UserIDKey]; !ok {
-			return payload, fmt.Errorf("token do not contains `%v`", j.UserIDKey)
-		}
-
-		if expiresAt, ok := payload[j.ExpiresAtKey]; !ok {
-			return payload, fmt.Errorf("token do not contains `%v`", j.ExpiresAtKey)
-		} else {
-			if ok, err = j.checkExpiresValid(Clock.GetUTCNow(), expiresAt); err != nil {
-				return payload, errors.Wrap(err, "parse token `expires_at` error")
-			} else if !ok {
-				return payload, fmt.Errorf("token expired at %v", payload[j.ExpiresAtKey])
-			}
-		}
-
-		return payload, nil
+	if !payload.VerifyExpiresAt(Clock.GetUTCNow().Unix(), true) { // exp must exists
+		return payload, fmt.Errorf("token expired")
 	}
-	return nil, errors.New("token not match MapClaims")
+	if _, ok = payload[j.JWTUserIDKey]; !ok {
+		err = fmt.Errorf("token must contains `%v`", j.JWTUserIDKey)
+	}
+	return payload, err
 }
 
 // GeneratePasswordHash generate hashed password by origin password
@@ -178,4 +249,105 @@ func GeneratePasswordHash(password []byte) ([]byte, error) {
 // ValidatePasswordHash validate password is match with hashedPassword
 func ValidatePasswordHash(hashedPassword, password []byte) bool {
 	return bcrypt.CompareHashAndPassword(hashedPassword, password) == nil
+}
+
+// DivideJWT jwt utils to generate and validate token.
+//
+// use seperate secret for each token
+type DivideJWT struct {
+	*DivideJWTCfg
+}
+
+// JWTUserModel load secret by uid
+type JWTUserModel interface {
+	GetUID() interface{}
+	LoadSecretByUID(uid interface{}) ([]byte, error)
+}
+
+// DivideJWTCfg configuration
+type DivideJWTCfg struct {
+	baseJWT
+}
+
+// NewDivideJWTCfg create new JwtCfg  with secret
+func NewDivideJWTCfg() *DivideJWTCfg {
+	jwt.TimeFunc = Clock.GetUTCNow
+	return &DivideJWTCfg{
+		baseJWT: baseJWT{
+			JWTSigningMethod: JWTSigningMethod,
+			JWTUserIDKey:     JWTUserIDKey,
+			JWTExpiresAtKey:  JWTExpiresAtKey,
+		},
+	}
+}
+
+// NewDivideJWT create new JWT with JwtCfg
+func NewDivideJWT(cfg *DivideJWTCfg) (*DivideJWT, error) {
+	if cfg.JWTUserIDKey == "" ||
+		cfg.JWTExpiresAtKey == "" ||
+		cfg.JWTSigningMethod == nil {
+		return nil, fmt.Errorf("configuration error")
+	}
+
+	return &DivideJWT{
+		DivideJWTCfg: cfg,
+	}, nil
+}
+
+// GenerateToken generate JWT token.
+// do not use `expires_at` & `uid` as keys.
+func (j *DivideJWT) GenerateToken(user JWTUserModel, expiresAt time.Time, payload map[string]interface{}) (tokenStr string, err error) {
+	jwtPayload := jwt.MapClaims{}
+	for k, v := range payload {
+		jwtPayload[k] = v
+	}
+	jwtPayload[j.JWTExpiresAtKey] = expiresAt.Unix()
+	jwtPayload[j.JWTUserIDKey] = user.GetUID()
+
+	token := jwt.NewWithClaims(JWTSigningMethod, jwtPayload)
+	var secret []byte
+	if secret, err = user.LoadSecretByUID(user.GetUID()); err != nil {
+		Logger.Error("try to load jwt secret by uid got error",
+			zap.Error(err),
+			zap.String("uid", fmt.Sprint(user.GetUID())))
+		return "", err
+	}
+	if tokenStr, err = token.SignedString(secret); err != nil {
+		return "", errors.Wrap(err, "try to signed token got error")
+	}
+	return tokenStr, nil
+}
+
+// Validate validate the token and return the payload
+//
+// if token is invalidate, err will not be nil.
+func (j *DivideJWT) Validate(user JWTUserModel, tokenStr string) (payload jwt.MapClaims, err error) {
+	Logger.Debug("Validate for token", zap.String("tokenStr", tokenStr))
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if method, ok := token.Method.(*jwt.SigningMethodHMAC); !ok || method != j.JWTSigningMethod {
+			return nil, errors.New("JWT method not allowd")
+		}
+		return user.LoadSecretByUID(user.GetUID())
+	})
+	if err != nil || !token.Valid {
+		// return after got payload
+		err = errors.Wrap(err, "token invalidate")
+	}
+
+	var ok bool
+	if payload, ok = token.Claims.(jwt.MapClaims); !ok {
+		return nil, errors.New("payload type not match `map[string]interface{}`")
+	}
+	if err != nil {
+		return payload, err
+	}
+
+	if !payload.VerifyExpiresAt(Clock.GetUTCNow().Unix(), true) { // exp must exists
+		return payload, fmt.Errorf("token expired")
+	}
+	if _, ok = payload[j.JWTUserIDKey]; !ok {
+		err = fmt.Errorf("token must contains `%v`", j.JWTUserIDKey)
+	}
+	return payload, err
 }
--- go.mod
+++ go.mod
@@ -11,7 +11,6 @@ require (
 	github.com/Shopify/goreferrer v0.0.0-20181106222321-ec9c9a553398 // indirect
 	github.com/Shopify/sarama v1.22.1
 	github.com/ajg/form v1.5.1 // indirect
-	github.com/appleboy/gofight/v2 v2.1.1 // indirect
 	github.com/aymerick/raymond v2.0.2+incompatible // indirect
 	github.com/bsm/sarama-cluster v2.1.15+incompatible
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
@@ -58,4 +57,4 @@ require (
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
 
-replace github.com/appleboy/gofight v2.0.0+incompatible => github.com/appleboy/gofight/v2 v2.1.1
+replace github.com/appleboy/gofight/v2 v2.1.1 => github.com/appleboy/gofight v2.0.0+incompatible
