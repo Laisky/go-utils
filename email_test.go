c4a460096e6db087c47e06086c63dac473b20398
Laisky <ppcelery@gmail.com>
2019-10-15 11:40:19 +0800

    ci: fix ci

--- email_test.go
+++ email_test.go
@@ -1,34 +1,9 @@
 package utils_test
 
 import (
-	"testing"
-
 	"github.com/Laisky/go-utils"
 	"github.com/Laisky/zap"
 )
-
-func TestSend(t *testing.T) {
-	utils.Settings.Set("debug", true)
-	if err := utils.Settings.Setup("/Users/laisky/repo/pateo/configs/go-ramjet"); err != nil {
-		t.Fatalf("setup config %+v", err)
-	}
-
-	sender := utils.NewMail(utils.Settings.GetString("email.host"), utils.Settings.GetInt("email.port"))
-	sender.Login(utils.Settings.GetString("email.username"), utils.Settings.GetString("email.password"))
-	origDry := utils.Settings.GetBool("dry")
-	utils.Settings.Set("dry", true)
-	if err := sender.Send(
-		"ramjet@pateo.com.cn",
-		"zhonghuacai@pateo.com.cn",
-		"Go-Ramjet",
-		"Laisky Cai",
-		"Go-Ramjet Alert",
-		"alert text",
-	); err != nil {
-		t.Errorf("got error %+v", err)
-	}
-	utils.Settings.Set("dry", origDry)
-}
 
 func ExampleMail() {
 	sender := utils.NewMail("smtp_host", 53)
