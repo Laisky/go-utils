ddf1fa4953e383dc01bc91f76a733bb2e55fdab3
Laisky <ppcelery@gmail.com>
2020-04-02 09:39:29 +0800

    feat: refactory jwt utils

--- jwt_test.go
+++ jwt_test.go
@@ -1,284 +1,164 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/Laisky/zap"
+	"github.com/dgrijalva/jwt-go"
 )
 
-func TestGenerateToken(t *testing.T) {
-	j, err := NewJWT([]byte("4738947328rh3ru23f32hf238f238fh28f"))
-	if err != nil {
-		t.Fatalf("got error: %+v", err)
-	}
-	expect := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.dS5DHPA_5vM-A4VIa8pFvag4EYp9PrRjmDtBth-EFYYJ5rprtUf83WTO8AQ1295AaGi0uES2bLmkQA8lQGI4Wg"
-
-	got, err := j.GenerateToken("laisky", time.Date(2119, 1, 1, 0, 0, 0, 0, time.UTC), map[string]interface{}{
-		"k1": "v1",
-		"k2": "v2",
-		"k3": "v3",
-	})
-	if err != nil {
-		t.Errorf("generate token error %+v", err)
-	}
-	if got != expect {
-		t.Errorf("expect %v, got %v", expect, got)
-	}
-}
-
-type jwtUser struct {
-	secret []byte
-}
-
-func (u *jwtUser) GetUID() interface{} {
-	return "laisky"
-}
-func (u *jwtUser) GetSecret() []byte {
-	return u.secret
-}
-
-func TestGenerateDivideToken(t *testing.T) {
-	j, err := NewDivideJWT()
-	if err != nil {
-		t.Fatalf("got error: %+v", err)
-	}
-	expect := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.dS5DHPA_5vM-A4VIa8pFvag4EYp9PrRjmDtBth-EFYYJ5rprtUf83WTO8AQ1295AaGi0uES2bLmkQA8lQGI4Wg"
-
-	u := &jwtUser{secret: []byte("4738947328rh3ru23f32hf238f238fh28f")}
-	got, err := j.GenerateToken(u, time.Date(2119, 1, 1, 0, 0, 0, 0, time.UTC), map[string]interface{}{
-		"k1": "v1",
-		"k2": "v2",
-		"k3": "v3",
-	})
-	if err != nil {
-		t.Errorf("generate token error %+v", err)
-	}
-	if got != expect {
-		t.Errorf("expect %v, got %v", expect, got)
-	}
-}
-
-const (
-	defaultUserIDKey  = "uid"
-	defaultExpiresKey = "exp"
+var (
+	es256PriByte = []byte(`-----BEGIN PRIVATE KEY-----
+MHcCAQEEIKBr4xv3gD85+ZAfgflb6y36PEwQjA+fD4w7QjIlxoD0oAoGCCqGSM49
+AwEHoUQDQgAEUfNN1nvU2g8yr058Fsvjx6k6sOdcqLW+xXwTysxo/xiZcW8fwQow
+CyxcGJv8r7OfHYB/FScm3jgOaNhabM6laQ==
+-----END PRIVATE KEY-----`)
+	es256PubByte = []byte(`-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEUfNN1nvU2g8yr058Fsvjx6k6sOdc
+qLW+xXwTysxo/xiZcW8fwQowCyxcGJv8r7OfHYB/FScm3jgOaNhabM6laQ==
+-----END PUBLIC KEY-----
+`)
+	secret = []byte("4738947328rh3ru23f32hf238f238fh28f")
 )
 
-func TestValidToken(t *testing.T) {
-	j, err := NewJWT(
-		[]byte("4738947328rh3ru23f32hf238f238fh28f"),
-		WithJWTUserIDKey(defaultUserIDKey),
-		WithJWTExpiresKey(defaultExpiresKey),
-	)
-	if err != nil {
-		t.Errorf("got error %+v", err)
-	}
-	expect := map[string]interface{}{
-		"k1":              "v1",
-		"k2":              "v2",
-		"k3":              "v3",
-		defaultUserIDKey:  "laisky",
-		defaultExpiresKey: time.Date(2119, 1, 1, 0, 0, 0, 0, time.UTC).UTC(),
-	}
-	t.Logf("exp: %v", expect["exp"])
-	// correct token
-	token := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.dS5DHPA_5vM-A4VIa8pFvag4EYp9PrRjmDtBth-EFYYJ5rprtUf83WTO8AQ1295AaGi0uES2bLmkQA8lQGI4Wg"
-
-	got, err := j.Validate(token)
-	if err != nil {
-		t.Fatalf("got error %+v", err)
-	}
-	t.Logf("got: %+v", got)
-	for k, ev := range expect {
-		if v, ok := got[k]; !ok {
-			t.Fatalf("key %v not exists in got", k)
-		} else {
-			if v == ev {
-				continue
-			}
-			t.Fatalf("value of key %v not match, expect %v, got %v", k, ev, v)
-		}
-	}
-
-	// check expires
-	token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.DhNK_cmiPkOUs2gU4X3Ue5utd0wHpyaCimnKSrrr4XQmdzgfKpaYbPzlouDa0KUVqDSmYPYaLAi3v6m1geV48g"
-	if got, err = j.Validate(token); err == nil {
-		t.Logf("got: %v", got)
-		t.Fatal("token should be expired")
-	} else if !strings.Contains(err.Error(), "Token is expired") {
-		t.Fatalf("expect invalidate error, bug got %+v:%+v", err, got)
-	}
-
-	// check without `exp`
-	token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJrMSI6InYxIiwiazIiOiJ2MiIsImszIjoidjMiLCJ1aWQiOiJsYWlza3kifQ.74A-PjmIj9Vqwfp8MWQGfeVkSxDbH0N2pA5Ru_r0au8YKhNsvk4H7BH0sz97-i0sf_0Izq-VhRqLQM2qP6qlWA"
-	if got, err = j.Validate(token); err == nil {
-		t.Fatalf("token should be error since of lack of `%v`", defaultExpiresKey)
-	} else if !strings.Contains(err.Error(), "unknown expires format") {
-		t.Fatalf("expect unknown expires format error, but got: %+v", got)
-	}
-
-	// check without `uid`
-	token = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyJ9.nF1_ySCLWUppYjgBLRMjBRtjfqZkaqaT8p3QaVjHlg7qBIRvXPVArdWsqRAKqpA1nAxwQjYnhVI9tOslK-M04w"
-	if got, err = j.Validate(token); err == nil {
-		t.Error("token should be error since of lack of `uid`")
-	} else if !strings.Contains(err.Error(), "must contains `uid`") {
-		t.Fatalf("expect invalidate error, bug got %+v", got)
-	}
-
-	// check different method
-	token = "eyJhbGciOiJIUzM4NCJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.SF7MS3drdHjQ2k1cDyiWspnDx6f0QiBpxT0B3NM0it1eHd01fJ25Zh2n8iH42DFa"
-	if got, err = j.Validate(token); err == nil {
-		t.Error("token should be error since of method incorrect`")
-	} else if !strings.Contains(err.Error(), "JWT method not allowd") {
-		t.Fatalf("expect method error, bug got %+v", got)
-	}
-	// invalidate method, but should return complete payload
-	t.Logf("got: %+v", got)
-	for k, ev := range expect {
-		if v, ok := got[k]; !ok {
-			t.Fatalf("key %v not exists in got", k)
-		} else {
-			if v == ev ||
-				k == defaultExpiresKey {
-				continue
-			}
-			t.Fatalf("value of key %v not match, expect %v, got %v", k, ev, v)
-		}
-	}
-}
-
-func TestValidDivideToken(t *testing.T) {
-	j, err := NewDivideJWT(
-		WithJWTUserIDKey(defaultUserIDKey),
-		WithJWTExpiresKey(defaultExpiresKey),
-	)
-	if err != nil {
-		t.Errorf("got error %+v", err)
-	}
-	expect := map[string]interface{}{
-		"k1":              "v1",
-		"k2":              "v2",
-		"k3":              "v3",
-		defaultUserIDKey:  "laisky",
-		defaultExpiresKey: time.Date(2119, 1, 1, 0, 0, 0, 0, time.UTC).UTC(),
-	}
-	t.Logf("exp: %v", expect["exp"])
-	// correct token
-	token := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.dS5DHPA_5vM-A4VIa8pFvag4EYp9PrRjmDtBth-EFYYJ5rprtUf83WTO8AQ1295AaGi0uES2bLmkQA8lQGI4Wg"
-
-	u := &jwtUser{secret: []byte("4738947328rh3ru23f32hf238f238fh28f")}
-	got, err := j.Validate(u, token)
-	if err != nil {
-		t.Fatalf("got error %+v", err)
-	}
-	t.Logf("got: %+v", got)
-	for k, ev := range expect {
-		if v, ok := got[k]; !ok {
-			t.Fatalf("key %v not exists in got", k)
-		} else {
-			if v == ev {
-				continue
-			}
-			t.Fatalf("value of key %v not match, expect %v, got %v", k, ev, v)
-		}
-	}
-
-	// check expires
-	token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.DhNK_cmiPkOUs2gU4X3Ue5utd0wHpyaCimnKSrrr4XQmdzgfKpaYbPzlouDa0KUVqDSmYPYaLAi3v6m1geV48g"
-	if got, err = j.Validate(u, token); err == nil {
-		t.Logf("got: %v", got)
-		t.Fatal("token should be expired")
-	} else if !strings.Contains(err.Error(), "token invalidate") {
-		t.Fatalf("expect invalidate error, bug got %+v", got)
-	}
-
-	// check without `exp`
-	token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJrMSI6InYxIiwiazIiOiJ2MiIsImszIjoidjMiLCJ1aWQiOiJsYWlza3kifQ.74A-PjmIj9Vqwfp8MWQGfeVkSxDbH0N2pA5Ru_r0au8YKhNsvk4H7BH0sz97-i0sf_0Izq-VhRqLQM2qP6qlWA"
-	if got, err = j.Validate(u, token); err == nil {
-		t.Fatalf("token should be error since of lack of `%v`", defaultExpiresKey)
-	} else if !strings.Contains(err.Error(), "unknown expires format") {
-		t.Fatalf("expect unknown expires format error, but got: %+v", got)
-	}
-
-	// check without `uid`
-	token = "eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyJ9.nF1_ySCLWUppYjgBLRMjBRtjfqZkaqaT8p3QaVjHlg7qBIRvXPVArdWsqRAKqpA1nAxwQjYnhVI9tOslK-M04w"
-	if got, err = j.Validate(u, token); err == nil {
-		t.Error("token should be error since of lack of `uid`")
-	} else if !strings.Contains(err.Error(), "must contains `uid`") {
-		t.Fatalf("expect invalidate error, bug got %+v", got)
-	}
-
-	// check different method
-	token = "eyJhbGciOiJIUzM4NCJ9.eyJleHAiOjQ3MDE5NzQ0MDAsImsxIjoidjEiLCJrMiI6InYyIiwiazMiOiJ2MyIsInVpZCI6ImxhaXNreSJ9.SF7MS3drdHjQ2k1cDyiWspnDx6f0QiBpxT0B3NM0it1eHd01fJ25Zh2n8iH42DFa"
-	if got, err = j.Validate(u, token); err == nil {
-		t.Error("token should be error since of method incorrect`")
-	} else if !strings.Contains(err.Error(), "JWT method not allowd") {
-		t.Fatalf("expect method error, bug got %+v", got)
-	}
-	// invalidate method, but should return complete payload
-	t.Logf("got: %+v", got)
-	for k, ev := range expect {
-		if v, ok := got[k]; !ok {
-			t.Fatalf("key %v not exists in got", k)
-		} else {
-			if v == ev ||
-				k == defaultExpiresKey {
-				continue
-			}
-			t.Fatalf("value of key %v not match, expect %v, got %v", k, ev, v)
-		}
-	}
+type testJWTClaims struct {
+	jwt.StandardClaims
 }
 
 func ExampleJWT() {
-	jwt, err := NewJWT([]byte("your secret key"))
+	secret = []byte("4738947328rh3ru23f32hf238f238fh28f")
+	j, err := NewJWT(
+		WithJWTSignMethod(SignMethodHS256),
+		WithJWTSecretByte(secret),
+	)
 	if err != nil {
-		Logger.Panic("try to init jwt got error", zap.Error(err))
+		Logger.Panic("new jwt", zap.Error(err))
 	}
 
-	// generate jwt token for user
-	// GenerateToken(userId string, expiresAt time.Time, payload map[string]interface{}) (tokenStr string, err error)
-	token, err := jwt.GenerateToken("laisky", time.Now().Add(7*24*time.Hour), map[string]interface{}{"display_name": "Laisky"})
-	if err != nil {
-		Logger.Error("try to generate jwt token got error", zap.Error(err))
-		return
+	type jwtClaims struct {
+		jwt.StandardClaims
 	}
-	fmt.Println("got token:", token)
 
-	// validate token
-	payload, err := jwt.Validate(token)
-	if err != nil {
-		Logger.Error("token invalidate")
-		return
+	claims := &jwtClaims{
+		jwt.StandardClaims{
+			Subject: "laisky",
+		},
 	}
-	fmt.Printf("got payload from token: %+v\n", payload)
+
+	// signing
+	token, err := j.Sign(claims)
+	if err != nil {
+		Logger.Panic("sign jwt", zap.Error(err))
+	}
+
+	// verify
+	claims = &jwtClaims{}
+	if err := j.ParseClaims(token, claims); err != nil {
+		Logger.Panic("sign jwt", zap.Error(err))
+	}
+
 }
 
-func ExampleDivideJWT() {
-	jwt, err := NewDivideJWT()
+func TestJWTSignAndVerify(t *testing.T) {
+	jwtES256, err := NewJWT(
+		WithJWTSignMethod(SignMethodES256),
+		WithJWTPubKeyByte(es256PubByte),
+		WithJWTPriKeyByte(es256PriByte),
+	)
 	if err != nil {
-		Logger.Panic("try to init jwt got error", zap.Error(err))
+		t.Fatalf("got error: %+v", err)
 	}
 
-	// generate jwt token for user
-	// GenerateToken(userId string, expiresAt time.Time, payload map[string]interface{}) (tokenStr string, err error)
-	u := &jwtUser{secret: []byte("secret for this user")}
-	token, err := jwt.GenerateToken(u, time.Now().Add(7*24*time.Hour), map[string]interface{}{"display_name": "Laisky"})
+	jwtHS256, err := NewJWT(
+		WithJWTSignMethod(SignMethodHS256),
+		WithJWTSecretByte(secret),
+	)
 	if err != nil {
-		Logger.Error("try to generate jwt token got error", zap.Error(err))
-		return
+		t.Fatalf("got error: %+v", err)
 	}
-	fmt.Println("got token:", token)
 
-	// validate token
-	payload, err := jwt.Validate(u, token)
-	if err != nil {
-		Logger.Error("token invalidate")
-		// you can get the payload even the token is invalidate
-		Logger.Info("got payload", zap.String("payload", fmt.Sprint(payload)))
-		return
+	for _, j := range []*JWT{
+		jwtES256,
+		jwtHS256,
+	} {
+
+		claims := &testJWTClaims{
+			StandardClaims: jwt.StandardClaims{
+				Subject:  "laisky",
+				Audience: "dune",
+			},
+		}
+
+		// test sign & parse
+		token, err := j.Sign(claims)
+		if err != nil {
+			t.Fatalf("generate token error %+v", err)
+		}
+		// expect := "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkdW5lIiwic3ViIjoibGFpc2t5In0.UtcJn1th7rvZNr0HLl6h5G8XE-sJLVSqyc96LYAFG42-p0ZAJJeDeE_9a5sp770hEaIXMtZSvVeeBQre90oTLA"
+		// if token != expect {
+		// 	t.Fatalf("expect %v,\n got %v", expect, token)
+		// }
+
+		claims = &testJWTClaims{}
+		if err = j.ParseClaims(token, claims); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if claims.Subject != "laisky" ||
+			claims.Audience != "dune" {
+			t.Fatal()
+		}
+
+		expired := Clock.GetUTCNow().Add(-time.Hour)
+		future := Clock.GetUTCNow().Add(time.Hour)
+
+		// test exp
+		claims = &testJWTClaims{
+			jwt.StandardClaims{
+				ExpiresAt: expired.Unix(),
+			},
+		}
+		claims.ExpiresAt = expired.Unix()
+		if token, err = j.Sign(claims); err != nil {
+			t.Fatalf("generate token error %+v", err)
+		}
+		if err = j.ParseClaims(token, claims); err != nil {
+			if !strings.Contains(err.Error(), "token is expired") {
+				t.Fatalf("must expired, got: %s", err.Error())
+			}
+		} else {
+			t.Fatalf("must expired")
+		}
+
+		// test issuerAt
+		claims = &testJWTClaims{
+			jwt.StandardClaims{
+				IssuedAt: future.Unix(),
+			},
+		}
+		claims.ExpiresAt = expired.Unix()
+		if token, err = j.Sign(claims); err != nil {
+			t.Fatalf("generate token error %+v", err)
+		}
+		if err = j.ParseClaims(token, claims); err != nil {
+			if !strings.Contains(err.Error(), "used before issued") {
+				t.Fatalf("must invalid, got: %s", err.Error())
+			}
+		} else {
+			t.Fatalf("must invalid")
+		}
 	}
-	fmt.Printf("got payload from token: %+v\n", payload)
+}
+
+func TestParseJWTTokenWithoutValidate(t *testing.T) {
+	token := "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJkdW5lIiwic3ViIjoibGFpc2t5In0.UtcJn1th7rvZNr0HLl6h5G8XE-sJLVSqyc96LYAFG42-p0ZAJJeDeE_9a5sp770hEaIXMtZSvVeeBQre90oTLA"
+	claims, err := ParseJWTTokenWithoutValidate(token)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	if claims["sub"] != "laisky" ||
+		claims["aud"] != "dune" {
+		t.Fatal()
+	}
 }
