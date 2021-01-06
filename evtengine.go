0d0f967153ba43733c819b05d1a0aa935487cb4c
Laisky <ppcelery@gmail.com>
2021-01-06 08:49:56 +0000

    feat: add `time` in Event

--- evtengine.go
+++ evtengine.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/Laisky/zap"
 	"github.com/pkg/errors"
@@ -17,30 +18,31 @@ const (
 // Event evt
 type Event struct {
 	Topic string
+	Time  time.Time
 	Meta  map[string]interface{}
 }
 
 // EventHandler function to handle event
 type EventHandler func(*Event)
 
-type evtHandler struct {
+type evtHandlerItem struct {
 	h    EventHandler
 	name string
 }
 
 type evtHandlers struct {
 	sync.RWMutex
-	hs []evtHandler
+	hs []evtHandlerItem
 }
 
-func (e *evtHandlers) Append(handlers ...evtHandler) *evtHandlers {
+func (e *evtHandlers) Append(handlers ...evtHandlerItem) *evtHandlers {
 	e.hs = append(e.hs, handlers...)
 
 	return e
 }
 
 func (e *evtHandlers) Remove(name string) *evtHandlers {
-	var hs []evtHandler
+	var hs []evtHandlerItem
 	for _, h := range e.hs {
 		if h.name != name {
 			hs = append(hs, h)
@@ -55,16 +57,16 @@ func (e *evtHandlers) Len() int {
 	return len(e.hs)
 }
 
-func (e *evtHandlers) Clone() (handlers []evtHandler) {
+func (e *evtHandlers) Clone() (handlers []evtHandlerItem) {
 	return append(handlers, e.hs...)
 }
 
 // EventEngine type of event store
 type EventEngine struct {
-	*LoggerType
+	*eventStoreManagerOpt
 	q chan *Event
 
-	// topic2hs map[topic][]evtHandler
+	// topic2hs map[topic][]evtHandlerItem
 	topic2hs *sync.Map
 }
 
@@ -72,6 +74,7 @@ type eventStoreManagerOpt struct {
 	msgBufferSize int
 	nfork         int
 	logger        *LoggerType
+	suppressPanic bool
 }
 
 // EventEngineOptFunc options for EventEngine
@@ -113,6 +116,14 @@ func WithEventEngineLogger(logger *LoggerType) EventEngineOptFunc {
 	}
 }
 
+// WithEventEngineSuppressPanic set whether supress event handler's panic
+func WithEventEngineSuppressPanic(suppressPanic bool) EventEngineOptFunc {
+	return func(opt *eventStoreManagerOpt) error {
+		opt.suppressPanic = suppressPanic
+		return nil
+	}
+}
+
 // NewEventEngine new event store manager
 func NewEventEngine(ctx context.Context, opts ...EventEngineOptFunc) (e *EventEngine, err error) {
 	opt := &eventStoreManagerOpt{
@@ -127,19 +138,21 @@ func NewEventEngine(ctx context.Context, opts ...EventEngineOptFunc) (e *EventEn
 	}
 
 	e = &EventEngine{
-		LoggerType: opt.logger,
-		q:          make(chan *Event, opt.msgBufferSize),
-		topic2hs:   &sync.Map{},
+		eventStoreManagerOpt: opt,
+		q:                    make(chan *Event, opt.msgBufferSize),
+		topic2hs:             &sync.Map{},
 	}
 
-	e.run(ctx, opt.nfork)
-	e.Logger.Debug("new event store",
+	taskChan := make(chan *eventRunChanItem, opt.msgBufferSize)
+	e.startRunner(ctx, opt.nfork, taskChan)
+	e.run(ctx, taskChan)
+	e.logger.Info("new event store",
 		zap.Int("nfork", opt.nfork),
 		zap.Int("buffer", opt.msgBufferSize))
 	return e, nil
 }
 
-func runHandlerWithoutPanic(h evtHandler, evt *Event) (err error) {
+func runHandlerWithoutPanic(h evtHandlerItem, evt *Event) (err error) {
 	defer func() {
 		if erri := recover(); erri != nil {
 			err = errors.Errorf("run event handler `%s` with evt `%s`: %+v", h.name, evt.Topic, erri)
@@ -150,21 +163,45 @@ func runHandlerWithoutPanic(h evtHandler, evt *Event) (err error) {
 	return nil
 }
 
-// Run start EventEngine
-func (e *EventEngine) run(ctx context.Context, nfork int) {
-	for i := 0; i < nfork; i++ {
-		logger := e.Logger.Named(strconv.Itoa(i))
-		go func() {
-			logger.Debug("start event store runner")
+type eventRunChanItem struct {
+	h   evtHandlerItem
+	evt *Event
+}
 
-			var evt *Event
+func (e *EventEngine) startRunner(ctx context.Context, nfork int, taskChan chan *eventRunChanItem) {
+	for i := 0; i < nfork; i++ {
+		logger := e.logger.Named(strconv.Itoa(i))
+		go func() {
 			for {
 				select {
 				case <-ctx.Done():
 					return
-				case evt = <-e.q:
-				}
+				case t := <-taskChan:
+					logger.Debug("trigger handler",
+						zap.String("evt", t.evt.Topic),
+						zap.String("handler", t.h.name))
 
+					if e.suppressPanic {
+						if err := runHandlerWithoutPanic(t.h, t.evt); err != nil {
+							logger.Error("panic", zap.Error(err))
+						}
+					} else {
+						t.h.h(t.evt)
+					}
+				}
+			}
+		}()
+	}
+}
+
+// Run start EventEngine
+func (e *EventEngine) run(ctx context.Context, taskChan chan *eventRunChanItem) {
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case evt := <-e.q:
 				hsi, ok := e.topic2hs.Load(evt.Topic)
 				if !ok || hsi == nil {
 					continue
@@ -175,21 +212,20 @@ func (e *EventEngine) run(ctx context.Context, nfork int) {
 				hsi.(*evtHandlers).RUnlock()
 
 				for _, h := range hs {
-					// if err := runHandlerWithoutPanic(h, evt); err != nil {
-					// 	logger.Error("panic", zap.Error(err))
-					// }
-
-					h.h(evt)
+					taskChan <- &eventRunChanItem{
+						h:   h,
+						evt: evt,
+					}
 				}
 			}
-		}()
-	}
+		}
+	}()
 }
 
 // Register register new handler to event store
 func (e *EventEngine) Register(topic, handlerName string, handler EventHandler) {
 	hs := &evtHandlers{
-		hs: []evtHandler{{
+		hs: []evtHandlerItem{{
 			name: handlerName,
 			h:    handler,
 		}},
@@ -199,6 +235,10 @@ func (e *EventEngine) Register(topic, handlerName string, handler EventHandler) 
 		actual.(*evtHandlers).Append(hs.hs...)
 		actual.(*evtHandlers).Unlock()
 	}
+
+	e.logger.Info("register handler",
+		zap.String("topic", topic),
+		zap.String("handler", handlerName))
 }
 
 // UnRegister delete handler in event store
@@ -212,9 +252,15 @@ func (e *EventEngine) UnRegister(topic, handlerName string) {
 
 		hsi.(*evtHandlers).Unlock()
 	}
+
+	e.logger.Info("unregister handler",
+		zap.String("topic", topic),
+		zap.String("handler", handlerName))
 }
 
 // Publish publish new event
 func (e *EventEngine) Publish(evt *Event) {
+	evt.Time = Clock.GetUTCNow()
 	e.q <- evt
+	e.logger.Debug("publish event", zap.String("event", evt.Topic))
 }
