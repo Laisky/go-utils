e836dccade519aa6f4cbb5bab2933e4d1f401929
Laisky.Cai <public@laisky.com>
2021-08-27 08:59:19 +0000

    docs: add some comment

--- fifo.go
+++ fifo.go
@@ -14,28 +14,42 @@ type fifoNode struct {
 //
 // paper: https://1drv.ms/b/s!Au45o0W1gVVLuNxYkPzfBo4fOssFPQ?e=TYxHKl
 type FIFO struct {
+	// head the node that before real head node
+	//
+	// head.next is the real head node
+	//
+	// unsafe.pointer will tell gc not to remove object in heap
 	head unsafe.Pointer
+	// tail maybe(maynot) the tail node in queue
 	tail unsafe.Pointer
 	len  int64
 }
 
+// add a dummy node to the queue to avoid contention
+// betweet head & tail when queue is empty
+//
+// dummyNode is the default value to unsafe.pointer as an empty pointer
+var dummyNode = &fifoNode{
+	d: "dummy",
+}
+
+func init() {
+	dummyNode.next = unsafe.Pointer(dummyNode)
+}
+
 // NewFIFO create a new FIFO queue
 func NewFIFO() *FIFO {
-	// add a dummy node to the queue to avoid contention betweet head & tail
-	// when queue is empty
-	dummyNonde := &fifoNode{
-		d: "laisky",
-	}
 	return &FIFO{
-		head: unsafe.Pointer(dummyNonde),
-		tail: unsafe.Pointer(dummyNonde),
+		head: unsafe.Pointer(dummyNode),
+		tail: unsafe.Pointer(dummyNode),
 	}
 }
 
 // Put put an data into queue's tail
 func (f *FIFO) Put(d interface{}) {
 	newNode := &fifoNode{
-		d: d,
+		d:    d,
+		next: unsafe.Pointer(dummyNode),
 	}
 	newAddr := unsafe.Pointer(newNode)
 
@@ -43,11 +57,12 @@ func (f *FIFO) Put(d interface{}) {
 	for {
 		tailAddr = atomic.LoadPointer(&f.tail)
 		tailNode := (*fifoNode)(tailAddr)
-		if atomic.CompareAndSwapPointer(&tailNode.next, unsafe.Pointer(uintptr(0)), newAddr) {
+		if atomic.CompareAndSwapPointer(&tailNode.next, unsafe.Pointer(dummyNode), newAddr) {
 			atomic.AddInt64(&f.len, 1)
 			break
 		}
 
+		// tail may not be the exact tail node, so we need to check again
 		atomic.CompareAndSwapPointer(&f.tail, tailAddr, atomic.LoadPointer(&tailNode.next))
 	}
 
@@ -61,7 +76,7 @@ func (f *FIFO) Get() interface{} {
 		headAddr := atomic.LoadPointer(&f.head)
 		headNode := (*fifoNode)(headAddr)
 		nextAddr := atomic.LoadPointer(&headNode.next)
-		if nextAddr == unsafe.Pointer(uintptr(0)) {
+		if nextAddr == unsafe.Pointer(dummyNode) {
 			// queue is empty
 			return nil
 		}
