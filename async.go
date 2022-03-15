c285721d03c78293d2d31813d2efdadf1ca635df
Laisky.Cai <public@laisky.com>
2022-03-15 03:50:06 +0000

    feat: async task add `Heartbeat`

--- async.go
+++ async.go
@@ -26,7 +26,6 @@ var (
 type AsyncTaskResult struct {
 	TaskID string          `json:"task_id"`
 	Status AsyncTaskStatus `json:"status"`
-	Exp    time.Time       `json:"exp"`
 	Data   string          `json:"data"`
 	Err    string          `json:"err"`
 }
@@ -37,6 +36,8 @@ type AsyncTaskStore interface {
 	New(ctx context.Context) (result *AsyncTaskResult, err error)
 	// Set set AsyncTaskResult
 	Set(ctx context.Context, taskID string, result *AsyncTaskResult) (err error)
+	// Heartbeat refresh async task's updated time to mark this task is still alive
+	Heartbeat(ctx context.Context, taskID string) (alived bool, err error)
 }
 
 // asyncTask async task
@@ -55,28 +56,45 @@ type asyncTask struct {
 	id     string
 	store  AsyncTaskStore
 	result *AsyncTaskResult
+	cancel func()
 }
 
 // NewTask new async task
 func NewAsyncTask(ctx context.Context, store AsyncTaskStore) (AsyncTask, error) {
+	ctx, cancel := context.WithCancel(ctx)
+
 	result, err := store.New(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "new async task result")
 	}
 
-	result.Exp = Clock.GetUTCNow().Add(time.Hour)
 	result.Status = AsyncTaskStatusPending
 	t := &asyncTask{
 		id:     result.TaskID,
 		store:  store,
 		result: result,
+		cancel: cancel,
 	}
 
 	if err := store.Set(ctx, t.id, t.result); err != nil {
 		return nil, errors.Wrap(err, "set async task result")
 	}
 
+	go t.heartbeat(ctx)
 	return t, nil
+}
+
+func (t *asyncTask) heartbeat(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		t.store.Heartbeat(ctx, t.id)
+		SleepWithContext(ctx, 10*time.Second)
+	}
 }
 
 // ID get task id
@@ -91,10 +109,6 @@ func (t *asyncTask) Status() AsyncTaskStatus {
 
 // SetDone set task done with result data
 func (t *asyncTask) SetDone(ctx context.Context, data string) (err error) {
-	if t.result.Exp.After(Clock.GetUTCNow()) {
-		return errors.Wrap(ErrAsyncTask, "task expired")
-	}
-
 	t.result.Status = AsyncTaskStatusDone
 	t.result.Data = data
 
@@ -107,10 +121,6 @@ func (t *asyncTask) SetDone(ctx context.Context, data string) (err error) {
 
 // SetError set task error with err message
 func (t *asyncTask) SetError(ctx context.Context, errMsg string) (err error) {
-	if t.result.Exp.After(Clock.GetUTCNow()) {
-		return errors.Wrap(ErrAsyncTask, "task expired")
-	}
-
 	t.result.Status = AsyncTaskStatusFailed
 	t.result.Err = errMsg
 
