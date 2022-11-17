2122d95a99c679053179d463f49367ebece83a71
Laisky.Cai <public@laisky.com>
2022-11-17 05:53:29 +0000

    feat: add `RSAEncrypt`

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -2,9 +2,14 @@
 package encrypt
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
 	"encoding/base64"
+	"io"
 	"math/big"
 
+	"github.com/Laisky/errors"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -44,4 +49,58 @@ func ParseBase642Big(raw string) (*big.Int, error) {
 	b := new(big.Int)
 	b.SetBytes(bb)
 	return b, nil
+}
+
+// RSAEncrypt encrypt by PKCS1v15
+//
+// canbe decrypt by RSADecrypt
+func RSAEncrypt(pubkey *rsa.PublicKey, plain []byte) (cipher []byte, err error) {
+	chunk := make([]byte, pubkey.Size()-11) // will padding 11 bytes
+	reader := bytes.NewReader(plain)
+	for {
+		n, err := reader.Read(chunk)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, errors.Wrap(err, "read chunk")
+		}
+
+		cipherChunk, err := rsa.EncryptPKCS1v15(rand.Reader, pubkey, chunk[:n])
+		if err != nil {
+			return nil, errors.Wrap(err, "encrypt chunkd")
+		}
+
+		cipher = append(cipher, cipherChunk...)
+	}
+
+	return cipher, nil
+}
+
+// RSADecrypt decrypt by rsa PKCS1v15
+//
+// only accept cipher encrypted by RSAEncrypt
+func RSADecrypt(prikey *rsa.PrivateKey, cipher []byte) (plain []byte, err error) {
+	chunk := make([]byte, prikey.Size())
+	reader := bytes.NewReader(cipher)
+	for {
+		n, err := reader.Read(chunk)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, errors.Wrap(err, "read chunk")
+		}
+
+		plainChunk, err := rsa.DecryptPKCS1v15(rand.Reader, prikey, chunk[:n])
+		if err != nil {
+			return nil, errors.Wrap(err, "encrypt chunkd")
+		}
+
+		plain = append(plain, plainChunk...)
+	}
+
+	return plain, nil
 }
---
+++ encrypt/encrypt_test.go
@@ -0,0 +1,26 @@
+// Package encrypt contains some useful tools to deal with encryption/decryption
+package encrypt
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRSAEncrypt(t *testing.T) {
+	prikey, err := NewRSAPrikey(RSAPrikeyBits3072)
+	require.NoError(t, err)
+
+	plain := make([]byte, 102400)
+	_, err = rand.Read(plain)
+	require.NoError(t, err)
+
+	cipher, err := RSAEncrypt(&prikey.PublicKey, plain)
+	require.NoError(t, err)
+
+	gotPlain, err := RSADecrypt(prikey, cipher)
+	require.NoError(t, err)
+
+	require.Equal(t, plain, gotPlain)
+}
