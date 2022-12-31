bb3243c3863c1d165debd4a3315d28252c3168aa
Laisky.Cai <public@laisky.com>
2022-12-31 06:01:21 +0000

    fix: add cmd version

--- cmd/root.go
+++ cmd/root.go
@@ -2,18 +2,22 @@
 package cmd
 
 import (
+	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/Laisky/errors"
 	"github.com/Laisky/zap"
 	"github.com/spf13/cobra"
 
-	"github.com/Laisky/go-utils/v3/log"
+	gutils "github.com/Laisky/go-utils/v3"
+	glog "github.com/Laisky/go-utils/v3/log"
 )
 
 var (
-	cmdDebug bool
+	cmdDebug   bool
+	cmdVersion bool
 )
 
 var rootCmd = &cobra.Command{
@@ -29,24 +33,32 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.ParseFlags(os.Args)
+
+	if cmdVersion {
+		fmt.Println(gutils.PrettyBuildInfo())
+		return
+	}
+
 	defer func() {
-		_ = log.Shared.Sync()
+		_ = glog.Shared.Sync()
 	}()
 	rand.Seed(time.Now().UnixNano())
 
 	if cmdDebug {
-		if err := log.Shared.ChangeLevel(log.LevelDebug); err != nil {
-			log.Shared.Panic("change logger level to debug", zap.Error(err))
+		if err := glog.Shared.ChangeLevel(glog.LevelDebug); err != nil {
+			glog.Shared.Panic("change logger level to debug", zap.Error(err))
 		}
 	}
 
 	if err := rootCmd.Execute(); err != nil {
-		log.Shared.Panic("parse command line arguments", zap.Error(err))
+		glog.Shared.Panic("parse command line arguments", zap.Error(err))
 	}
 }
 
 func init() {
 	rootCmd.PersistentFlags().BoolVar(&cmdDebug, "debug", false, "debug")
+	rootCmd.PersistentFlags().BoolVarP(&cmdVersion, "version", "v", false, "print version")
 }
 
 // NoExtraArgs make sure every args has been processed
