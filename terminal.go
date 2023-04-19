847b5dc34fc3d10bf67cd61b2dec362d548bb07f
Laisky <github@laisky.com>
2023-04-19 06:41:52 +0000

    feat: Refactor `InputPassword` function to accept validator and retry invalid passwords.
    - Add password validator functionality to `InputPassword` in `terminal.go`
    - Allow retrying of invalid passwords in `InputPassword`
    - Improve user experience and security in password input process

--- terminal.go
+++ terminal.go
@@ -11,19 +11,22 @@ import (
 
 // InputPassword reads password from stdin input
 // and returns it as a string.
-func InputPassword(hint string) (string, error) {
+func InputPassword(hint string, validator func(string) bool) (passwd string, err error) {
 	fmt.Printf("%s: ", hint)
 
-	// ReadPassword reads the password from the terminal
-	// with echo disabled which ensures the input is not
-	// displayed on the screen.
-	bytepw, err := term.ReadPassword(syscall.Stdin)
-	if err != nil {
-		return "", errors.Wrap(err, "read input password")
-	}
+	for {
+		bytepw, err := term.ReadPassword(syscall.Stdin)
+		if err != nil {
+			return "", errors.Wrap(err, "read input password")
+		}
 
-	// Convert byte slice to string
-	return string(bytepw), nil
+		if validator != nil && !validator(string(bytepw)) {
+			fmt.Printf("password is invalid, try again: ")
+			continue
+		}
+
+		return string(bytepw), nil
+	}
 }
 
 // InputYes require user input `y` or `Y` to continue
